74bd36895c89cbb80fffd10a8372948ad2a89ad0
imam76 <asepimam335@gmail.com>
2022-01-12 14:32:42 +0700

    add antd or ant design

--- libs/todo/src/lib/todo.spec.tsx
+++ libs/todo/src/lib/todo.spec.tsx
@@ -2,14 +2,19 @@ import { render } from '@testing-library/react';
 
 import Todo from './todo';
 
-describe('Todo', () => {
+describe("Test", () => {
+  beforeAll(() => {
+    Object.defineProperty(window, "matchMedia", {
+      value: jest.fn(() => {
+        return {
+          matches: true,
+          addListener: jest.fn(),
+          removeListener: jest.fn()
+        };
+      })
+    });
+  });
   it('should render successfully', () => {
-    const arr = [
-      {
-        id: 1,
-        name: 'Asep'
-      }
-    ]
     const { baseElement } = render(<Todo prefix='Books' />);
     expect(baseElement).toBeTruthy();
   });
